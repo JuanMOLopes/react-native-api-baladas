f88b79e9291168079c4290ac4faabd2363c69554
JuanMOLopes <juan.oliiveira2007@gmail.com>
2025-09-17 13:30:11 -0300

    Initialize backend with Express and CORS dependencies

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const cors = require('cors')
 const app = express();
 const port = 3000;
 // Middleware para analisar o corpo das requisições em JSON
 app.use(express.json());
+
+// usando o cors para conseguir requisições no front-end
+app.use(cors())
+
 // Importando as rotas do cliente
 const clienteRoutes = require("./routes/baladaRoutes");
 // Usando as rotas do cliente com o prefixo '/baladas'
